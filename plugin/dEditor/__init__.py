319a1fa17684490b27fa61dd0f5f97fa99c8473c
iamhyc <sudofree@163.com>
2017-12-28 10:50:51 +0800

    update import exception handle

--- plugin/dEditor/__init__.py
+++ plugin/dEditor/__init__.py
@@ -4,4 +4,8 @@ __editor_dir__ = __os_none__.path.dirname(__file__)
 
 for __editor_file__ in __os_none__.listdir(__editor_dir__):
 	if __os_none__.path.isdir(__os_none__.path.join(__editor_dir__, __editor_file__)):
-		__import__(__editor_file__, globals(), locals())
+		try:
+			__import__(__editor_file__, globals(), locals())
+		except Exception as e:
+			print(e)
+			pass
--- plugin/dSystem/__init__.py
+++ plugin/dSystem/__init__.py
@@ -4,4 +4,8 @@ __system_dir__ = __os_none__.path.dirname(__file__)
 
 for __system_file__ in __os_none__.listdir(__system_dir__):
 	if __os_none__.path.isdir(__os_none__.path.join(__system_dir__, __system_file__)):
-		__import__(__system_file__, globals(), locals())
+		try:
+			__import__(__system_file__, globals(), locals())
+		except Exception as e:
+			print(e)
+			pass
