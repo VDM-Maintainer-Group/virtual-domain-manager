2a99110d0704a261ac22e1b512e5634f6706d54e
iamhyc <sudofree@163.com>
2017-12-28 20:07:11 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+PWD=${0%/*}
+printf "\t      [Virtual Domain Manager]      \n"
+sudo echo
 
 # dependency check
 function pkg_check(){
@@ -9,22 +12,29 @@ function pkg_check(){
 }
 
 function grep_json_item(){
-	data=`(cat "config.json")|grep $1|awk -F ":" '{print $2}'`
+	data=`(cat $PWD/config.json)|grep $1|awk -F ":" '{print $2}'`
 	data=${data%\",*}
 	echo ${data#\"*}
 }
 
 printf "\t======== Dependency Check ========\n"
-sudo echo
 pkg_check "python-wnck"
 pkg_check "rsync"
 echo
 
+printf "\t======== Compile & Build ========\n"
+# make in bin/
+# run python compile
+rm -rf $PWD/build && mkdir $PWD/build
+rsync -rq --exclude-from $PWD'/exclude.rsync' $PWD/* $PWD/build
+echo
+
 printf "\t========   Installation   ========\n"
+read -p "Press Enter to continue..."
 # python setup.py install
 item=`grep_json_item "install-dir"`
 # install to config.install_dir
-# sudo cp -r ./ $item
+sudo mkdir -p $item && sudo cp -rf $PWD/build/* $item
 # add install_dir to path
 if [[ ! $PATH =~ $item ]]; then
 	echo "export PATH=\$PATH:$item" >> ~/.bashrc
