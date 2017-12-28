b4f79ac2e735d57bc189b354a30dc49895cc7b11
iamhyc <sudofree@163.com>
2017-12-28 22:10:11 +0800

    abandon python setuptools

--- install.sh
+++ install.sh
@@ -8,7 +8,22 @@ function pkg_check(){
 	pkg=$1;
 	printf "[PENDING]\t%s " $pkg
 	sudo apt-get install -y $pkg 1> /dev/null
+	if [[ $? -ne 0 ]]; then
+		printf "[apt-get]\t%s fail.\n" $pkg
+	 	exit -1
+	fi
 	printf "\r[INSTALLED]\t%s\n" $pkg
+}
+
+function pip_check(){
+	pkg=$1;
+	printf "[PENDING]\tpython-%s " $pkg
+	sudo pip install $pkg 1> /dev/null
+	if [[ $? -ne 0 ]]; then
+		printf "[pip]\t%s fail.\n" $pkg
+	 	exit -1
+	fi
+	printf "\r[INSTALLED]\tpython-%s\n" $pkg
 }
 
 function grep_json_item(){
@@ -18,8 +33,13 @@ function grep_json_item(){
 }
 
 printf "\t======== Dependency Check ========\n"
-pkg_check "python-wnck"
 pkg_check "rsync"
+pkg_check "python-pip"
+pkg_check "python-wnck"
+pkg_check "python-dbus"
+pip_check 'termcolor'
+pip_check 'crcmod'
+pip_check 'greenlet'
 echo
 
 printf "\t======== Compile & Build ========\n"
