b43bdc6aa05e8b4039f2530062603b9ebd79c86c
iamhyc <sudofree@163.com>
2021-01-09 00:08:08 +0800

    complete plugin manager argparse

--- core/pyvdm/plugin_manager.py
+++ core/pyvdm/plugin_manager.py
@@ -23,54 +23,67 @@ class PluginManager:
     def __init__(self):
         pass
 
-    def install(self):
+    def install(self, url):
         pass
 
-    def uninstall(self):
+    def uninstall(self, names):
         pass
 
-    def list(self):
+    def list(self, name=[]):
         pass
 
-    def run(self):
+    def run(self, name, function):
         pass
 
     pass
 
-def main(args):
-    # print('The command <{}> is not supported.'.format(args.command[0]))
+def execute(pm, command, args):
+    if command=='install':
+        pm.install(args.url)
+    elif command=='uninstall':
+        pm.uninstall(args.names)
+    elif command=='list':
+        pm.list()
+    elif command=='run':
+        pm.run(args.plugin_name, args.plugin_function)
+    else:
+        print('The command <{}> is not supported.'.format(command))
+    pass
+
+def init_subparsers(subparsers):
+    p_install = subparsers.add_parser('install',
+        help='install a new VDM plugin.')
+    p_install.add_argument('url', metavar='plugin_file',
+        help='the path to the plugin file in .zip format')
+    #
+    p_uninstall = subparsers.add_parser('uninstall',
+        help='uninstall VDM plugins.')
+    p_uninstall.add_argument('names', metavar='plugin_names', nargs='+',
+        help='the plugin name(s) to uninstall.')
+    #
+    p_list = subparsers.add_parser('list',
+        help='list information of installed VDM plugins.')
+    p_list.add_argument('names', metavar='plugin_names', nargs='*',
+        help='the specified plugin name(s) to list.')
+    #
+    p_run = subparsers.add_parser('run',
+        help='run the function of an existing plugin.')
+    p_run.add_argument('plugin_name',
+        help='plugin name')
+    p_run.add_argument('plugin_function',
+        help='plugin function name')
     pass
 
 if __name__ == '__main__':
     try:
-        # help="silent without debug information"
         parser = argparse.ArgumentParser(
             description='VDM Plugin Manager.')
         subparsers = parser.add_subparsers(dest='command')
-        #
-        p_install = subparsers.add_parser('install',
-            help='install a new VDM plugin.')
-        p_install.add_argument('url', metavar='plugin_file',
-            help='the path to the plugin file in .zip format')
-        #
-        p_uninstall = subparsers.add_parser('uninstall',
-            help='uninstall VDM plugins.')
-        p_uninstall.add_argument('names', metavar='plugin_names', nargs='+',
-            help='the plugin name(s) to uninstall.')
-        #
-        p_list = subparsers.add_parser('list',
-            help='list information of installed VDM plugins.')
-        #
-        p_run = subparsers.add_parser('run',
-            help='run the function of an existing plugin.')
-        p_run.add_argument('plugin_name',
-            help='plugin name')
-        p_run.add_argument('plugin_function',
-            help='plugin function name')
+        init_subparsers(subparsers)
         #
         args = parser.parse_args()
-        print(args)
-        main(args)
+        pm = PluginManager()
+        execute(pm, args.command, args)
     except Exception as e:
         raise e#print(e)
     finally:
