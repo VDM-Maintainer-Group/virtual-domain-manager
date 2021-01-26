3d67874a4626d02076ae964150abc1225687be6d
iamhyc <sudofree@163.com>
2021-01-26 15:57:42 +0800

    fix setuptools build

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 from setuptools import find_packages, setup
+from core.pyvdm.utils import WorkSpace
 
 if __name__ == '__main__':
-    print(find_packages(where='./build'))
-    setup(
-        name = 'pyvdm',
-        version = '0.1.0',
-        description = 'Pyvdm is a simple python implementation for VDM.',
-        url = 'https://github.com/VDM-Maintainer-Group/virtual-domain-manager',
-        author = 'iamhyc',
-        author_email = 'sudofree@163.com',
-        #
-        install_requires = ['PyQt5'],
-        packages = find_packages(where='./build'),
-        entry_points = {
-            'console_scripts': [
-                'pyvdm = package.core.manager:main',
-                'pyvdm-tray = package.gui.tray:main'
-            ]
-        }
-    )
+    with WorkSpace('./build') as ws:
+        print( find_packages() )
+        setup(
+            name = 'pyvdm',
+            version = '0.1.0',
+            description = 'Pyvdm is a simple python implementation for VDM.',
+            url = 'https://github.com/VDM-Maintainer-Group/virtual-domain-manager',
+            author = 'iamhyc',
+            author_email = 'sudofree@163.com',
+            #
+            install_requires = ['PyQt5'],
+            packages = find_packages(),
+            entry_points = {
+                'console_scripts': [
+                    'pyvdm = package.core.manager:main',
+                    'pyvdm-tray = package.gui.tray:main'
+                ]
+            }
+        )
+        pass
