dc2b66567a0e02173dc94623a9dcc180a616a18d
iamhyc <sudofree@163.com>
2021-01-27 13:33:05 +0800

    build pyvdm with assets

--- setup.py
+++ setup.py
@@ -3,23 +3,24 @@ from setuptools import find_packages, setup
 from core.pyvdm.utils import WorkSpace
 
 if __name__ == '__main__':
-    with WorkSpace('./build') as ws:
-        print( find_packages() )
-        setup(
-            name = 'pyvdm',
-            version = '0.1.0',
-            description = 'Pyvdm is a simple python implementation for VDM.',
-            url = 'https://github.com/VDM-Maintainer-Group/virtual-domain-manager',
-            author = 'iamhyc',
-            author_email = 'sudofree@163.com',
-            #
-            install_requires = ['PyQt5'],
-            packages = find_packages(),
-            entry_points = {
-                'console_scripts': [
-                    'pyvdm = package.core.manager:main',
-                    'pyvdm-tray = package.gui.tray:main'
-                ]
-            }
-        )
-        pass
+    setup(
+        name = 'pyvdm',
+        version = '0.1.0',
+        description = 'Pyvdm is a simple python implementation for VDM.',
+        url = 'https://github.com/VDM-Maintainer-Group/virtual-domain-manager',
+        author = 'iamhyc',
+        author_email = 'sudofree@163.com',
+        #
+        install_requires = ['PyQt5'],
+        package_dir = {'': 'build'},
+        packages = find_packages(where='build'),
+        package_data = {
+            "pyvdm": ["assets/*"],
+        },
+        entry_points = {
+            'console_scripts': [
+                'pyvdm = pyvdm.core.manager:main',
+                'pyvdm-tray = pyvdm.gui.tray:main'
+            ]
+        }
+    )
