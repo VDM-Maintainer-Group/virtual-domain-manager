b6ab2778c97a731376462570468db8dc466d156a
iamhyc <sudofree@163.com>
2021-03-10 21:24:44 +0800

    add transition scene widget

--- gui/pyvdm/TransitionSceneWidget.py
+++ gui/pyvdm/TransitionSceneWidget.py
@@ -1,21 +1,84 @@
 #!/usr/bin/env python3
-import sys
-import pkg_resources
-from PyQt5.QtCore import (Qt, QRect, QSize)
-from PyQt5.QtGui import (QIcon, QMovie)
-from PyQt5.QtWidgets import (QApplication, QWidget, QDesktopWidget, QLabel,
-                        QLayout, QGridLayout, QPlainTextEdit, QSizePolicy)
+'''Mind Flash
+This is *mf*, a flash pass your mind.
+You Write, You Listen.
+'''
+import sys, pkg_resources
+from PyQt5.QtCore import (Qt, QPoint, QSize, QTimer, QThread)
+from PyQt5.QtGui import (QFont, QFontMetrics, QIcon, QMovie)
+from PyQt5.QtWidgets import (QApplication, QLabel, QWidget, QDesktopWidget,
+                    QLayout, QGridLayout)
 
-ASSETS = lambda _: pkg_resources.resource_filename(__name__, _)
+ASSETS = lambda _: pkg_resources.resource_filename('pyvdm', 'assets/'+_)
 
-class TransitionSceneWidget(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent, Qt.Dialog)
+class SceneManager(QWidget):
+    def __init__(self, parent, filename):
+        super().__init__(parent)
+        self.parent = parent
+        self.styleHelper()
+        self.setScene(filename)
+        self.show()
+        self.start()
+        pass
+    
+    def styleHelper(self):
+        self.grid = QGridLayout()
+        self.grid.setSpacing(0)
+        self.grid.setContentsMargins(0,0,0,0)
+        # self.grid.setAlignment(Qt.AlignCenter)
+        #
+        self.label = QLabel('Transition Scene', self)
+        self.grid.addWidget(self.label, 0, 0)
+        self.setLayout(self.grid)
+        #
+        self.setFixedSize( self.parent.size() )
+        self.setStyleSheet('''
+            QWidget {
+                background: rgba(255,254,249, 0.96)
+            }
+        ''')
+        pass
+
+    def setScene(self, filename):
+        self.movie = QMovie( ASSETS(filename) )
+        self.label.setMovie(self.movie)
+        pass
+
+    def start(self):
+        print(self.size(), self.label.size())
+        if self.movie: self.movie.start()
+        pass
+
+    def stop(self):
+        if self.movie: self.movie.stop()
         pass
 
     pass
 
-if __name__ == "__main__":
+class TransitionSceneWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        # set fullscreen size on default display
+        cp = QDesktopWidget().availableGeometry().size()
+        self.setFixedSize(cp)
+        self.w_scene  = SceneManager(self, 'MOV-Trans-BNA.gif')
+        # set main window layout as grid
+        self.grid = QGridLayout()
+        self.grid.setSpacing(0)
+        self.grid.setContentsMargins(0,0,0,0)
+        self.grid.addWidget(self.w_scene, 0, 0)
+        self.setLayout(self.grid)
+        self.resize(self.sizeHint())
+        # set window style
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.setFocus()
+        self.show()
+        pass
+
+    pass
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w_ts = TransitionSceneWidget()
+    main_ts = TransitionSceneWidget()
     sys.exit(app.exec_())
