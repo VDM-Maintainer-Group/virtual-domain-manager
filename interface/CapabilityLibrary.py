5aa94322a7cb9ee3c1937086209ef2007fa1b47c
iamhyc <sudofree@163.com>
2021-05-01 20:59:16 +0800

    next for async send/recv

--- interface/CapabilityLibrary.py
+++ interface/CapabilityLibrary.py
@@ -4,10 +4,12 @@ import os, socket, struct, asyncio
 import json, base64
 from enum import Enum
 from functools import wraps
-from multiprocessing import shared_memory
+from multiprocessing import shared_memory, Process
 
+SHM_REQ_MAX_SIZE = 10*1024   #10KB
+SHM_RES_MAX_SIZE = 1024*1024 #1MB
 VDM_SERVER_PORT = 42000
-VDM_CLIENT_ID_LEN = 32
+VDM_CLIENT_ID_LEN = 16
 GET_RANDOM_ID = lambda: ''.join( random.choices(string.hexdigits, k=VDM_CLIENT_ID_LEN) )
 
 class __COMMAND(Enum): #2-byte
@@ -21,21 +23,40 @@ class __COMMAND(Enum): #2-byte
 
 class __STATUS(Enum): #1-byte
     #loop operation is lockless safe
-    SERVER_ON   = 0x01 #b0001
-    SERVER_DONE = 0x00 #b0000
-    CLIENT_ON   = 0x02 #b0010
-    CLIENT_DONE = 0x03 #b0011
-    pass
-
-class __HEADER:
+    SERVER_ON   = 0b0001
+    SERVER_DONE = 0b0000
+    CLIENT_ON   = 0b0010
+    CLIENT_DONE = 0b0011
     pass
 
 class ShmManager:
-    def __init__(self, __id) -> None:
-        shared_memory.SharedMemory(name=__id)
+    #req_header: ['status':1B, 'seq':4B, 'command':2B, 'size':2B]
+    #res_header: ['status':1B, 'seq':4B, 'size':3B]
+    def __init__(self, _id) -> None:
+        self.id = _id
+        self.shm_req = None
+        self.shm_res = shared_memory.SharedMemory(create=True, size=SHM_RES_MAX_SIZE)
+        self.shm_res.buf[0] = __STATUS.CLIENT_DONE
+        pass
+
+    def run(self) -> None:
+        self.shm_req = shared_memory.SharedMemory(name=self.id)
+        assert(self.shm_req[0] == __STATUS.SERVER_DONE)
+        #TODO:
         pass
 
     def is_alive(self) -> bool:
+        pass
+
+    def close(self) -> None:
+        self.shm_res.close()
+        self.shm_res.unlink()
+        if self.shm_req is not None:
+            try:
+                self.shm_req.close()
+                self.shm_req.unlink()
+            except:
+                pass
         pass
 
     def request(self, command:__COMMAND, *args, **kwargs):
@@ -44,7 +65,11 @@ class ShmManager:
     pass
 
 class CapabilityHandle:
-    def __init__(self, server: ShmManager, spec: dict):
+    def __init__(self, server: ShmManager, name):
+        spec = server.request(__COMMAND.REGISTER, name)
+        if spec is None:
+            raise Exception('Invalid Capability.')
+        #
         self.server = server
         self.spec = spec
         pass
@@ -58,7 +83,6 @@ class CapabilityHandle:
             #   - wrap request method
             @wraps(name)
             def _wrapper(*args, **kargs):
-                #TODO:
                 pass
             return _wrapper
         else:
@@ -90,19 +114,25 @@ class CapabilityLibrary:
         #
         _addr = ('', VDM_SERVER_PORT)
         _sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        __server = ShmManager(self.__id)
         try:
             _sock.connect(_addr)
             _sock.sendall(self.__id)
             _tmp = _sock.recv(VDM_CLIENT_ID_LEN+1) #+1 for '\0'
             if _tmp==self.__id:
-                self.__server = ShmManager(self.__id)
+                self.__server = __server
+                self.__server.run()
             else:
                 raise Exception('Invalid Connection: %s'%_tmp)
         except Exception as e:
+            __server.close()
+            _sock.close()
             raise e
         pass
 
     def disconnect(self) -> None:
+        if self.__server is None:
+            return
         if not self.__server.is_alive():
             return
         #
@@ -119,11 +149,11 @@ class CapabilityLibrary:
         if name in self.capability.keys():
             return self.capability[name]
         #
-        spec = self.__server.request(__COMMAND.REGISTER, name)
-        if spec is None:
+        try:
+            _item = CapabilityHandle(self.server, name)
+        except:
             return None
         else:
-            _item = CapabilityHandle(self.server, spec)
             self.capability.update( {name:_item} )
             return _item
         pass
