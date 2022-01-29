7c4afd4a90b467cf181fd4c5bbac889f2c376eb0
iamhyc <sudofree@163.com>
2022-01-29 20:49:25 +0800

    init control panel

---
+++ gui/pyvdm/ControlPanel.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+from pyvdm.core.manager import CoreManager
+from PyQt5.QtCore import (Qt, QThread, pyqtSignal, pyqtSlot)
+from PyQt5.QtWidgets import (QApplication, QLabel, QWidget, QTabWidget,
+                    QDesktopWidget, QLayout, QGridLayout)
+from PyQt5.QtMultimedia import (QAudioDeviceInfo, QSoundEffect)
+
+class MainTabWidget(QTabWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        pass
+    pass
+
+class DMTabWidget(QTabWidget):
+    def __init__(self, parent, dm):
+        super().__init__(parent)
+        self.parent = parent
+        self.dm = dm
+        pass
+    pass
+
+class CMTabWidget(QTabWidget):
+    def __init__(self, parent, cm):
+        super().__init__(parent)
+        self.parent = parent
+        self.cm = cm
+        pass
+    pass
+
+class PMTabWidget(QTabWidget):
+    def __init__(self, parent, pm):
+        super().__init__(parent)
+        self.parent = parent
+        self.pm = pm
+        pass
+    pass
+
+class ControlPanelClass(QWidget):
+    def __init__(self, parent=None, core=None):
+        super().__init__(parent)
+        self.core = core if core else CoreManager()
+        #
+        self.styleHelper()
+        pass
+
+    def styleHelper(self):
+        pass
+
+    pass
+
+if __name__== '__main__':
+    pass
